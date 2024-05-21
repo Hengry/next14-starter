90cf0ba417ff4eaed77292afd93f2e85c2bc7e09
Heng Lee <hengry1213@gmail.com>
2024-05-21 12:31:55 +0800

    feat: add img.ly

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,40 @@
 'use client';
+import { useEffect } from 'react';
+import { UIEvent, PhotoEditorSDKUI, Tool } from 'photoeditorsdk';
+
 import TestButton from '@/components/TestButton';
 import useCounterStore from '@/store/counterStore';
+
+const initEditor = async () => {
+  const editor = await PhotoEditorSDKUI.init({
+    container: '#editor',
+    image: 'https://i.imgur.com/LvTDRcJ.jpeg', // Image url or Image path relative to assets folder
+    // Please replace this with your license: https://img.ly/dashboard
+    license: '',
+    tools: [Tool.BRUSH],
+  });
+  console.log('PhotoEditorSDK for Web is ready!');
+  editor.on(UIEvent.EXPORT, (imageSrc) => {
+    console.log('Exported ', imageSrc);
+  });
+};
 
 export default function Home() {
   const counts = useCounterStore((state) => state.counts);
 
+  useEffect(() => {
+    initEditor();
+  });
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen items-center justify-between p-24 h-screen w-screen'>
       <TestButton>test</TestButton>
       <div>{counts}</div>
+      <div
+        id='editor'
+        className='bg-white flex-1 h-full relative'
+        // style={{ position: 'relative', height: '800px', width: '600px' }}
+      />
     </main>
   );
 }
