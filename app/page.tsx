aeb24dde71720aed069154c68da4592c8865e053
Heng Lee <hengry1213@gmail.com>
2024-05-21 14:34:59 +0800

    feat: add example for customizing export

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,49 @@
 'use client';
-import { useEffect } from 'react';
-import { UIEvent, PhotoEditorSDKUI, Tool } from 'photoeditorsdk';
+import React from 'react';
+import {
+  UIEvent,
+  PhotoEditorSDKUI,
+  Tool,
+  ImageFormat,
+  ExportFormat,
+} from 'photoeditorsdk';
 
 import TestButton from '@/components/TestButton';
 import useCounterStore from '@/store/counterStore';
 
-const initEditor = async () => {
-  const editor = await PhotoEditorSDKUI.init({
-    container: '#editor',
-    image: 'https://i.imgur.com/LvTDRcJ.jpeg', // Image url or Image path relative to assets folder
-    // Please replace this with your license: https://img.ly/dashboard
-    license: '',
-    tools: [Tool.BRUSH],
-  });
-  console.log('PhotoEditorSDK for Web is ready!');
-  editor.on(UIEvent.EXPORT, (imageSrc) => {
-    console.log('Exported ', imageSrc);
-  });
-};
-
 export default function Home() {
   const counts = useCounterStore((state) => state.counts);
 
-  useEffect(() => {
+  const initEditor = React.useCallback(async () => {
+    const editor = await PhotoEditorSDKUI.init({
+      container: '#editor',
+      image: 'https://i.imgur.com/LvTDRcJ.jpeg', // Image url or Image path relative to assets folder
+      // Please replace this with your license: https://img.ly/dashboard
+      license: '',
+      // tools: [Tool.BRUSH],
+      export: {
+        image: {
+          enableDownload: false,
+          format: ImageFormat.JPEG,
+          exportType: ExportFormat.BLOB,
+        },
+      },
+    });
+    console.log('PhotoEditorSDK for Web is ready!');
+    // editor.export({
+    //   format: ImageFormat.JPEG, // `image/png` or `image/jpeg`
+    //   exportType: ExportFormat.BLOB, // `image`, `data-url` or `blob`
+    //   quality: 0.9, // 0.1 - 1.0, defines the quality of jpeg images
+    //   enableDownload: false, // boolean, enable or disable the automatic file download
+    //   preventExportEvent: true, // boolean, enable or disable the UIEvent.EXPORT which is called on every export
+    // });
+    editor.on(UIEvent.EXPORT, (imageSrc) => {
+      console.log('Exported ', imageSrc);
+      editor.close();
+    });
+  }, []);
+
+  React.useEffect(() => {
     initEditor();
   });
 
